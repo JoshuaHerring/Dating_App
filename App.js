b5fe93d5185db277f9eb54c92a8271196c7f1a3a
JoshuaHerring <jdherring2222@gmail.com>
2023-11-06 19:02:13 -0700

    basic text, image, input, scrollview leanred

--- App.js
+++ App.js
@@ -1,22 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {View, Text, Image, ScrollView, TextInput, StyleSheet} from 'react-native';
+
+// Documentation for adding multiple screens
+// https://reactnative.dev/docs/navigation
 
 export default function App() {
+  const [text, setText] = useState()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ScrollView style={styles.container_outer} contentContainerStyle={styles.container_inner}>
+      <Image style={styles.logo} source={require("./assets/logo.png")}></Image>
+      <Text style={styles.paragraph}>Welcome To Together Journey. The dating app that gives you a starting point to your relationship</Text>
+      <TextInput style={styles.input} onChangeText={(newText) => {setText(newText)}} placeholder=' What is your name?'></TextInput>
+      <Text style={styles.paragraph}>{text}</Text>
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container_outer: {
+    backgroundColor: '#ec4104',
   },
+  container_inner: {
+    flex: 1,
+    alignItems: 'center',
+    marginLeft: 20,
+    marginRight: 20,
+    // justifyContent: 'center'
+  },
+  logo: {
+    marginTop:50,
+    backgroundColor: '#fff',
+    borderRadius:100,
+    width: 200,
+    height: 200
+  },
+  paragraph: {
+    color:'white',
+    marginTop:20
+  },
+  input: {
+    marginTop:20,
+    width:'100%',
+    backgroundColor: 'white',
+  }
 });
 
 
